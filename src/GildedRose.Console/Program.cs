b47c200c4755147f7b87150b90cbe96cbd10044f
Kristian Dam-Jensen <Systemic33@users.noreply.github.com>
2018-01-29 13:32:17 +0100

    Move filling of default supplies into a separate supplymanager
    makes it possible to unit test the default supply values.

--- src/GildedRose.Console/Program.cs
+++ src/GildedRose.Console/Program.cs
@@ -6,6 +6,7 @@ namespace GildedRose.Console
     {
         IList<Item> Items;
         IInventoryManager InventoryManager;
+        ISupplyManager SupplyManager;
 
         static void Main(string[] args)
         {
@@ -14,35 +15,23 @@ namespace GildedRose.Console
             var app = new Program()
             {
                 InventoryManager = new LegacyInventoryManager(),
-                Items = PurchaseItemsFromSuppliers()
+                SupplyManager = new SupplyManager()
             };
+            app.PurchaseNewSupplies();
 
             app.UpdateQuality();
 
             System.Console.ReadKey();
         }
 
-        public static IList<Item> PurchaseItemsFromSuppliers()
-        {
-            return new List<Item>
-            {
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item
-                    {
-                        Name = "Backstage passes to a TAFKAL80ETC concert",
-                        SellIn = 15,
-                        Quality = 20
-                    },
-                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
-        }
-
         public void UpdateQuality()
         {
             Items = InventoryManager.ProcessInventory(Items);
+        }
+
+        public void PurchaseNewSupplies()
+        {
+            Items = SupplyManager.PurchaseDefaultSupplies();
         }
     }
 
