3f97382280afa21606ad733277feaf41bdd16a80
Kristian Dam-Jensen <Systemic33@users.noreply.github.com>
2018-01-29 13:38:07 +0100

    Add tests for the two fundamental inventory rules
    Unless othewise specified all items degrade quality twice as fast after passing the sell day, but quality never goes negative.

--- src/GildedRose.Tests/TestAssemblyTests.cs
+++ src/GildedRose.Tests/TestAssemblyTests.cs
@@ -1,16 +1,63 @@
 using System;
 using Xunit;
+using GildedRose.Console;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GildedRose.Tests
 {
 
-    public class UnitTest1
+    public class InventoryTests
     {
+        IList<Item> items;
+        IInventoryManager inventoryManager;
+
+        public InventoryTests()
+        {
+            items = new List<Item>();
+            inventoryManager = new LegacyInventoryManager();
+        }
 
         [Fact]
-        public void Test1()
+        public void Purchase_Fills_Inventory()
         {
+            Assert.Empty(items);
+            items = new SupplyManager().PurchaseDefaultSupplies();
+            Assert.NotEmpty(items);
+        }
 
+        [Fact]
+        public void Items_With_Passed_SellBy_Degrades_Twice_As_Fast()
+        {
+            items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 0, Quality = 20},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 0, Quality = 7},
+            };
+
+            items = inventoryManager.ProcessInventory(items);
+
+            var vest = items.Single(x => x.Name == "+5 Dexterity Vest");
+            var elixir = items.Single(x => x.Name == "Elixir of the Mongoose");
+
+            Assert.True(vest.Quality == 18);
+            Assert.True(elixir.Quality == 5);
+        }
+
+        [Fact]
+        public void Quality_Is_Never_Negative()
+        {
+            items = new SupplyManager().PurchaseDefaultSupplies();
+            var maxSellIn = items.Select(x => x.SellIn).OrderByDescending(x => x).First() + 1;
+
+            for (int i = 0; i < maxSellIn; i++)
+            {
+                items = inventoryManager.ProcessInventory(items);
+            }
+            foreach (var item in items)
+            {
+                Assert.True(item.Quality >= 0);
+            }
         }
     }
 }
